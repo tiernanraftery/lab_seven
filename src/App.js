f8d522158fabe564e51b13c5ef61fe75c9bcac31
unknown <martin.kenirons@gmit.ie>
2024-10-04 14:07:13 +0000

    exercise 4 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/header';
 import Content from './components/content';
