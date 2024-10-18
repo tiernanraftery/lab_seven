ef98489d5935c95e2d79fbc47ae60e0d7885f4f0
unknown <martin.kenirons@gmit.ie>
2024-10-18 14:01:34 +0000

    lab4 complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,7 +1,30 @@
-const Create = ()=>{
-    return(
+import { useState } from "react";
+
+const Create = () => {
+
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
+    }
+
+    return (
         <div>
             <h3>Hello from create component!</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Movie Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Add Movie"></input>
+                </div>
+            </form>
         </div>
     );
 }
