2fb6ae85c04eb530e6e206d891ee0588cd14fb27
unknown <martin.kenirons@gmit.ie>
2024-11-08 14:44:53 +0000

    exercise 3

---
+++ BackEnd/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+          "Title": "Avengers: Infinity War (server)",
+          "Year": "2018",
+          "imdbID": "tt4154756",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+          "Title": "Captain America: Civil War (server)",
+          "Year": "2016",
+          "imdbID": "tt3498820",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        },
+        {
+          "Title": "World War Z (server)",
+          "Year": "2013",
+          "imdbID": "tt0816711",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        }
+      ];
+    res.status(200).json({movies})
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- src/components/read.js
+++ src/components/read.js
@@ -7,7 +7,7 @@ const Read = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+    axios.get('http://localhost:4000/api/movies')
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
